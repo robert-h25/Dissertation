a40394f2a90ead25bfe8e3f887c9900291661355
Robert Helps <sc21rh@leeds.ac.uk>
2024-02-13 19:58:46 +0000

    added skeleton functions

---
+++ patterns.py
@@ -0,0 +1,34 @@
+# Functions which find certain patterns in a Sudoku puzzle
+
+def sole_technique(grid):
+    return False
+
+def unique_candidate(grid):
+    return False
+
+def BRC_interaction(grid):
+    return False
+
+def block_block_interaction(grid):
+    return False
+
+def naked_subset(grid):
+    return False
+
+def hidden_subset(grid):
+    return False
+
+def X_wing(grid):
+    return False
+
+def swordfish(grid):
+    return False
+
+def forcing_chain(grid):
+    return False
+
+def XY_wing(grid):
+    return False
+
+def unique_rectangle(grid):
+    return False
