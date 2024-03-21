51d9d2dde7c494dcd9c1315557750849d79fd0e6
Robert Helps <sc21rh@leeds.ac.uk>
2024-03-21 18:22:55 +0000

    displays score and patterns in html

--- static/do_patterns.js
+++ static/do_patterns.js
@@ -7,6 +7,12 @@ document.getElementById("get_patterns").addEventListener("click", function() {
             // Handle the response from the server
             console.log("Patterns:", data.patterns);
             console.log("Score:", data.score);
+            // If we recieve data display it
+            if (data.patterns.length > 0) {
+                display_data(data.patterns,data.score);
+            } else {
+                console.log("No patterns found.");
+            }
             
         })
         .catch(error => {
@@ -14,3 +20,40 @@ document.getElementById("get_patterns").addEventListener("click", function() {
         });
         
 });
+
+// TODO: format the data for the display
+// Function to display the patterns and score
+function display_data(patterns,score) {
+    const patternsCon = document.getElementById("patterns");
+    patternsCon.innerHTML = ""; 
+
+    //list of all pattern names in this order
+    const patternNames = [
+        "Sole Technique",
+        "Unique Candidate",
+        "BRC Interaction",
+        "Block-Block Interaction",
+        "Naked Subset",
+        "Hidden Subset",
+        "X-Wing",
+        "Swordfish",
+        "Forcing Chain",
+        "XY-Wing",
+        "Unique Rectangle"
+    ];
+
+    // add patterns
+    patterns.forEach((value, index) => {
+        const patternDiv = document.createElement("div");
+        patternDiv.classList.add("pattern");
+        // get name and display data for each
+        patternDiv.innerHTML = `<strong>${patternNames[index]}:</strong> ${value}`;
+        // add to the div
+        patternsCon.appendChild(patternDiv);
+    });
+
+    //add score
+    const scoreDiv = document.createElement("div");
+    scoreDiv.innerHTML = `<strong>Score:</strong> ${score}`;
+    patternsCon.appendChild(scoreDiv);
+}
